53ad943568e23e6452d2070beafe59329d8090aa
Dann <manrique_jd@hotmail.com>
2016-06-25 23:33:13 -0500

    Add Logo and Title into Toolbar

--- app/src/main/java/com/manrique/daniel/moneybuddy/MainActivity.java
+++ app/src/main/java/com/manrique/daniel/moneybuddy/MainActivity.java
@@ -6,14 +6,15 @@ import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
     }
 }
