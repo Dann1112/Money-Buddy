58e1650803a0dd6b56e259f4ba8c8524566d6057
Dann <manrique_jd@hotmail.com>
2016-06-26 01:21:19 -0500

    Add Date functionality
    Add date functionality to main_menu layout
    DateView gets date from system

--- app/src/main/java/com/manrique/daniel/moneybuddy/MainActivity.java
+++ app/src/main/java/com/manrique/daniel/moneybuddy/MainActivity.java
@@ -1,20 +1,29 @@
 package com.manrique.daniel.moneybuddy;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
-    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        setToolbar();
+        loadFragment(savedInstanceState, new MainMenuFragment());
+
+    }
+
+    private void setToolbar() {
+        Toolbar toolbar;
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    private void loadFragment(Bundle state, Fragment fragment) {
 
         // Check that the activity is using the layout version with
         // the fragment_container FrameLayout
@@ -23,15 +32,15 @@ public class MainActivity extends AppCompatActivity {
             // However, if we're being restored from a previous state,
             // then we don't need to do anything and should return or else
             // we could end up with overlapping fragments.
-            if (savedInstanceState != null) {
+            if (state != null) {
                 return;
             }
 
             // Create a new Fragment to be placed in the activity layout
-            MainMenuFragment mainMenu = new MainMenuFragment();
+
 
             // Add the fragment to the 'fragment_container' FrameLayout
-            getSupportFragmentManager().beginTransaction().add(R.id.mainFrame, mainMenu).commit();
+            getSupportFragmentManager().beginTransaction().add(R.id.mainFrame, fragment).commit();
         }
 
     }
--- app/src/main/java/com/manrique/daniel/moneybuddy/MainMenuFragment.java
+++ app/src/main/java/com/manrique/daniel/moneybuddy/MainMenuFragment.java
@@ -1,14 +1,38 @@
 package com.manrique.daniel.moneybuddy;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainMenuFragment extends android.support.v4.app.Fragment {
 
+    TextView dateView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setDate();
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.main_menu,container,false);
+        View view = inflater.inflate(R.layout.main_menu, container, false);
+        dateView = (TextView) view.findViewById(R.id.date);
+        dateView.setText(new SimpleDateFormat("dd / MMMM / yyyy", Locale.getDefault()).format(new Date()));
+        return view;
+
+
+    }
+
+    private void setDate() {
+
+
     }
 }
