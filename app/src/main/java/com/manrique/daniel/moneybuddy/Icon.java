9dcd239cfea955cc9fac396f6a4e49dd0cc19330
Dann <manrique_jd@hotmail.com>
2016-06-27 00:59:27 -0500

    Fix Main Menu Items
    Add icon with same width and height
    Set new layout
    Make layout compatible with -v21

---
+++ app/src/main/java/com/manrique/daniel/moneybuddy/Icon.java
@@ -0,0 +1,26 @@
+package com.manrique.daniel.moneybuddy;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class Icon extends ImageView {
+
+    public Icon(Context context) {
+        super(context);
+    }
+
+    public Icon(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Icon(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(heightMeasureSpec, heightMeasureSpec);
+
+    }
+}
--- app/src/main/java/com/manrique/daniel/moneybuddy/MainMenuFragment.java
+++ app/src/main/java/com/manrique/daniel/moneybuddy/MainMenuFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -14,11 +15,11 @@ import java.util.Locale;
 public class MainMenuFragment extends android.support.v4.app.Fragment {
 
     TextView dateView;
+    ImageView imageView;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setDate();
     }
 
     @Override
@@ -26,13 +27,14 @@ public class MainMenuFragment extends android.support.v4.app.Fragment {
         View view = inflater.inflate(R.layout.main_menu, container, false);
         dateView = (TextView) view.findViewById(R.id.date);
         dateView.setText(new SimpleDateFormat("dd - MMM - yy", Locale.getDefault()).format(new Date()));
+        imageView = (ImageView) view.findViewById(R.id.income);
+
+
         return view;
 
 
     }
 
-    private void setDate() {
 
 
-    }
 }
