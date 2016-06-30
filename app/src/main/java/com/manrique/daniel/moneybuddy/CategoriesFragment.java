b348c76451a51b2a479610eeb2d598bd5aad440a
Dann <manrique_jd@hotmail.com>
2016-06-29 23:19:02 -0500

    Clean CategoriesFragment
    Create new variables for adapters

--- app/src/main/java/com/manrique/daniel/moneybuddy/CategoriesFragment.java
+++ app/src/main/java/com/manrique/daniel/moneybuddy/CategoriesFragment.java
@@ -15,18 +15,24 @@ public class CategoriesFragment extends Fragment {
     private final String[] amounts = {" $300.30", "$ 64.50", "52.89"};
     private final int[] colors = {Color.GREEN, Color.RED, Color.YELLOW};
     private final int[] icons = {R.drawable.balance, R.drawable.right_arrow, R.drawable.left_arrow};
-    private ExpandableHeightGridView grid;
 
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         if (isAdded()) {
+
+            CategoryAdapter adapter =
+                    new CategoryAdapter(this.getContext(), titles, amounts, colors, icons);
+
+            ExpandableHeightGridView grid;
+
             View view = inflater.inflate(R.layout.categories, container, false);
+
             grid = (ExpandableHeightGridView) view.findViewById(R.id.grid_usual);
-
-            grid.setAdapter(new CategoryAdapter(this.getContext(), titles, amounts, colors, icons));
+            grid.setAdapter(adapter);
             grid.setExpanded(true);
-            grid = (ExpandableHeightGridView) view.findViewById(R.id.grid_unusual);
 
-            grid.setAdapter(new CategoryAdapter(this.getContext(), titles, amounts, colors, icons));
+
+            grid = (ExpandableHeightGridView) view.findViewById(R.id.grid_unusual);
+            grid.setAdapter(adapter);
             grid.setExpanded(true);
 
             return view;
