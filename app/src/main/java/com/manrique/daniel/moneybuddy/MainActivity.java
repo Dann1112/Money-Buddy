d4871e4ca5d9960041b94641a66ff6d2d4d662c8
Dann <manrique_jd@hotmail.com>
2016-06-30 00:41:06 -0500

    Add Fragment Manager to change between fragments

--- app/src/main/java/com/manrique/daniel/moneybuddy/MainActivity.java
+++ app/src/main/java/com/manrique/daniel/moneybuddy/MainActivity.java
@@ -1,11 +1,18 @@
 package com.manrique.daniel.moneybuddy;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager manager = getSupportFragmentManager();
+    FragmentTransaction trans = manager.beginTransaction();
+
+    CategoriesFragment categories = new CategoriesFragment();
+    MainMenuFragment main_menu = new MainMenuFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         setToolbar();
-        loadFragment(savedInstanceState, new CategoriesFragment());
+        trans.add(R.id.mainFrame, main_menu);
+        trans.commit();
 
     }
 
@@ -21,28 +29,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar;
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-    }
-
-    private void loadFragment(Bundle state, Fragment fragment) {
-
-        // Check that the activity is using the layout version with
-        // the fragment_container FrameLayout
-        if (findViewById(R.id.mainFrame) != null) {
-
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
-            if (state != null) {
-                return;
-            }
-
-            // Create a new Fragment to be placed in the activity layout
-
-
-            // Add the fragment to the 'fragment_container' FrameLayout
-            getSupportFragmentManager().beginTransaction().add(R.id.mainFrame, fragment).commit();
-        }
-
     }
 
 }
