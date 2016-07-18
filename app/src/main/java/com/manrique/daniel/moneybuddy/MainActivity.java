6a5d1c2ebf4fe1830ce1c8fe8be7b9e653752bc1
Dann <manrique_jd@hotmail.com>
2016-07-18 13:18:16 -0500

    Fix landscape layouts

--- app/src/main/java/com/manrique/daniel/moneybuddy/MainActivity.java
+++ app/src/main/java/com/manrique/daniel/moneybuddy/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,16 +18,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setToolbar();
         trans.add(R.id.mainFrame, main_menu);
         trans.commit();
 
     }
 
-    private void setToolbar() {
-        Toolbar toolbar;
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-    }
 
 }
