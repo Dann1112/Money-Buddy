491fa583e6598a0e27eca040c45d20d59833539f
Dann <manrique_jd@hotmail.com>
2016-07-29 13:32:11 -0500

    Add DBOpenHealper
    Fix Contract Constants

--- app/src/main/java/com/manrique/daniel/moneybuddy/DatabaseContract.java
+++ app/src/main/java/com/manrique/daniel/moneybuddy/DatabaseContract.java
@@ -11,7 +11,7 @@ public final class DatabaseContract {
     /* Inner classes that define the tables contents */
     public static final class Category implements BaseColumns {
         public static final String TABLE_NAME = "category";
-        public static final String COLUMN_NAME_CATEGORY_ID = "category_id";
+        public static final String _ID = "category_id";
         public static final String COLUMN_NAME_TITLE = "title";
         public static final String COLUMN_NAME_ICON = "icon";
         public static final String COLUMN_NAME_COLOR = "color";
@@ -19,14 +19,14 @@ public final class DatabaseContract {
 
     public static final class CategoryItem implements BaseColumns {
         public static final String TABLE_NAME = "category_item";
-        public static final String COLUMN_NAME_CATEGORY_ITEM_ID = "category_item_id";
-        public static final String COLUMN_NAME_CATEGORY_ID = "category_id";
+        public static final String _ID = "category_item_id";
+        public static final String COLUMN_NAME_CATEGORY_KEY = "category_id";
         public static final String COLUMN_NAME_DESCRIPTION = "desc";
     }
 
     public static final class Income implements BaseColumns {
         public static final String TABLE_NAME = "income";
-        public static final String COLUMN_NAME_INCOME_ID = "income_id";
+        public static final String _ID = "income_id";
         public static final String COLUMN_NAME_DATE = "date";
         public static final String COLUMN_NAME_DESCRIPTION = "description";
         public static final String COLUMN_NAME_AMOUNT = "amount";
@@ -35,9 +35,10 @@ public final class DatabaseContract {
     public static final class Expense implements BaseColumns {
         public static final String TABLE_NAME = "expense";
         public static final String COLUMN_NAME_DATE = "date";
-        public static final String COLUMN_NAME_CATEGORY_ID = "category_id";
-        public static final String COLUMN_NAME_CATEGORY_ITEM_ID = "category_item_id";
+        public static final String _ID = "category_id";
+        public static final String COLUMN_NAME_CATEGORY_ITEM_KEY = "category_item_id";
         public static final String COLUMN_NAME_AMOUNT = "amount";
+
     }
 
 }
