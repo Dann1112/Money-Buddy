c97d2b496eeb12a66d462af9d3fa80aa13b05f12
Dann <manrique_jd@hotmail.com>
2016-08-10 13:53:19 -0500

    Add data change in Main Menu through arrows

--- app/src/main/java/com/manrique/daniel/moneybuddy/UI/MainMenuFragment.java
+++ app/src/main/java/com/manrique/daniel/moneybuddy/UI/MainMenuFragment.java
@@ -6,19 +6,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.manrique.daniel.moneybuddy.R;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.Calendar;
 
 public class MainMenuFragment extends android.support.v4.app.Fragment
         implements View.OnClickListener {
 
     TextView dateView;
     Button income_btn, expense_btn, balance_btn;
+    ImageView lastDay, nextDay;
+    Calendar calendar;
+    SimpleDateFormat sdf;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,16 +31,29 @@ public class MainMenuFragment extends android.support.v4.app.Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.main_menu, container, false);
-        dateView = (TextView) view.findViewById(R.id.date);
-        dateView.setText(new SimpleDateFormat("dd - MMM - yy", Locale.getDefault()).format(new Date()));
+
 
         income_btn = (Button) view.findViewById(R.id.income_button);
         expense_btn = (Button) view.findViewById(R.id.expense_button);
         balance_btn = (Button) view.findViewById(R.id.balance_button);
+        dateView = (TextView) view.findViewById(R.id.date);
+        nextDay = (ImageView) view.findViewById(R.id.date_right_arrow);
+        lastDay = (ImageView) view.findViewById(R.id.date_left_arrow);
+
+        calendar = Calendar.getInstance();
+        sdf = new SimpleDateFormat("MMMM dd, yyyy");
+
+        dateView.setText(sdf.format(calendar.getTime()));
+
+
+
 
         income_btn.setOnClickListener(this);
         expense_btn.setOnClickListener(this);
         balance_btn.setOnClickListener(this);
+        nextDay.setOnClickListener(this);
+        lastDay.setOnClickListener(this);
+
 
         return view;
     }
@@ -48,14 +64,11 @@ public class MainMenuFragment extends android.support.v4.app.Fragment
         IncomeFragment incomeFragment = new IncomeFragment();
         CategoriesFragment categoriesFragment = new CategoriesFragment();
         Bundle args = new Bundle();
-        args.putString("day",
-                new SimpleDateFormat("dd", Locale.getDefault()).format(new Date()));
-        args.putString("month",
-                new SimpleDateFormat("MMMM", Locale.getDefault()).format(new Date()));
-        args.putString("year",
-                new SimpleDateFormat("yyyy", Locale.getDefault()).format(new Date()));
-        args.putString("monthNumber",
-                new SimpleDateFormat("MM", Locale.getDefault()).format(new Date()));
+        args.putString("day", new SimpleDateFormat("dd").format(calendar.getTime()));
+        args.putString("month", new SimpleDateFormat("MMMM").format(calendar.getTime()));
+        args.putString("year", new SimpleDateFormat("yyyy").format(calendar.getTime()));
+        args.putString("monthNumber", new SimpleDateFormat("MM").format(calendar.getTime()));
+
 
         incomeFragment.setArguments(args);
         categoriesFragment.setArguments(args);
@@ -70,6 +83,15 @@ public class MainMenuFragment extends android.support.v4.app.Fragment
                     .addToBackStack(null).commit();
         } else if (view == balance_btn) {
             this.getContext().deleteDatabase("moneyBuddy.db");
+        } else if (view == nextDay) {
+            calendar.add(Calendar.DAY_OF_WEEK, 1);
+            dateView.setText(sdf.format(calendar.getTime()));
+
+
+        } else if (view == lastDay) {
+            calendar.add(Calendar.DAY_OF_WEEK, -1);
+            dateView.setText(sdf.format(calendar.getTime()));
+
         }
 
     }
